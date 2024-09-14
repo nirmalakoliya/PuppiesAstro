8d6338524ba7d75b25200ce058acd3d924d04c4b
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-09-14 13:39:23 +0530

    change font in dogvideo page

--- src/components/DogVideo.tsx
+++ src/components/DogVideo.tsx
@@ -25,12 +25,12 @@ export default function DogVideo() {
         </div>
 
         <div className="video-content z-10 space-y-4">
-          <h1 className="mb-4 text-4xl font-bold text-white md:text-5xl lg:text-6xl">
+          <h1 className="dogvideofont mb-4 text-4xl font-bold text-white md:text-5xl lg:text-6xl">
             OUR PUPPIES FOR <br /> SALE FROM
             <span className="text-purple-500"> $495!</span>
           </h1>
 
-          <div className="flex justify-center">
+          <div className=" PuppyDealsFonts flex justify-center">
             <button className="flex items-center justify-center rounded-full bg-white px-6 py-2 text-lg text-black shadow-md transition duration-300 hover:bg-purple-500 hover:text-white">
               Puppy deals <GiSniffingDog size={30} className="ml-2" />
             </button>
